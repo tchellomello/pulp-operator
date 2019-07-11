6dc45b5fdf22630b1c8d9346b22a80a73f7314ff
Marcelo Mello <tchello.mello@gmail.com>
2019-07-11 16:09:05 -0400

    Bumped operator to 0.8.1 and forced namespace

--- up.sh
+++ up.sh
@@ -3,8 +3,8 @@
 # TODO: Check if all of these are needed.
 # TODO: Check if these should only ever be run once; or require
 # special logic to update
-kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
-kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
+kubectl apply -n pulp -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
+kubectl apply -n pulp -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
 
 kubectl apply -n pulp -f deploy/service_account.yaml
 kubectl apply -n pulp -f deploy/role.yaml
