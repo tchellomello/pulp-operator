a6572d9f22be9485e63aed3b0e49a25cbeaf5449
Marcelo Mello <tchello.mello@gmail.com>
2019-07-18 13:27:35 -0400

    workaround problem wiht postgres and fixed down script

--- down.sh
+++ down.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-
-# TODO: Check if all of these are needed.
-# TODO: Check if these should only ever be run once; or require
-# special logic to update
-kubectl delete -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
-kubectl delete -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
-
-kubectl delete -f deploy/service_account.yaml
-kubectl delete -f deploy/role.yaml
-kubectl delete -f deploy/role_binding.yaml
 kubectl delete -f deploy/operator.yaml
 
-# clean pods
+# clean stuff
 kubectl -n pulp delete deployments postgres pulp-api pulp-content pulp-operator pulp-resource-manager pulp-worker redis
 kubectl -n pulp delete service postgres pulp-api pulp-content redis
 kubectl -n pulp delete pvc postgres-data pulp-file-storage redis-data
 kubectl -n pulp delete configmap pulp-server
 
+kubectl -n pulp delete -f deploy/service_account.yaml
+kubectl -n pulp delete -f deploy/role_binding.yaml
+kubectl -n pulp delete -f deploy/role.yaml
+
+kubectl -n pulp delete -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
+kubectl -n pulp delete -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
