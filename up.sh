e97b0932d53cbe40cb13a8dcf5088fb27861f094
Marcelo Mello <tchello.mello@gmail.com>
2019-07-19 18:08:52 -0400

    Added cmd to create ns

--- up.sh
+++ up.sh
@@ -3,6 +3,7 @@
 # TODO: Check if all of these are needed.
 # TODO: Check if these should only ever be run once; or require
 # special logic to update
+kubectl create ns pulp
 kubectl apply -n pulp -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
 kubectl apply -n pulp -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
 
