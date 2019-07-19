ea66d48cf709638811793e7aa4064f44bb38ea05
Marcelo Mello <tchello.mello@gmail.com>
2019-07-19 01:51:47 -0400

    Fixed pulp down.sh

--- down.sh
+++ down.sh
@@ -2,7 +2,7 @@
 kubectl -n pulp delete -f deploy/operator.yaml
 
 # clean stuff
-kubectl -n pulp delete deployments postgres pulp-api pulp-content pulp-operator pulp-resource-manager pulp-worker redis
+kubectl -n pulp delete deployments postgres pulp-api pulp-content pulp-resource-manager pulp-worker redis
 kubectl -n pulp delete service postgres pulp-api pulp-content redis
 kubectl -n pulp delete pvc postgres-data pulp-file-storage redis-data
 kubectl -n pulp delete configmap pulp-server
